9f05192a70ed9997e9b8f56ac5a440602fce0494
Vikram Naik <119391188+Vikram043@users.noreply.github.com>
2025-05-27 13:08:19 +0530

    Add files via upload

---
+++ Apply/index.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  function multiplyNumbers(a, b) {
+    return multiply.apply(null, [a, b]);
+  }
+  
+  
+  console.log(multiplyNumbers(4, 5));
+  
